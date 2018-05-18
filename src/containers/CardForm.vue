e6e59fcf00b7d79b347dad2f16d750efb8c6f4b1
XoldpandaX <rembo-87@yandex.ru>
2018-05-18 14:04:07 +0700

    added new components && added new styles

---
+++ src/containers/CardForm.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .card-form
+    form
+      card
+</template>
+
+<script>
+  import Card from './../components/Card.vue';
+
+  export default {
+    name: "card-form",
+    components: {
+      Card
+    }
+  }
+</script>
--- src/views/AddNewCard.vue
+++ src/views/AddNewCard.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   section.add-new-card.page
     .container
-      h1 dfsd
+      h1 Add New Card
+      .add-new-card__row
+        card-form
 </template>
 
 <script>
+  import CardForm from './../containers/CardForm.vue';
 
   export default {
     name: 'add-new-card',
     components: {
-
+      CardForm
     }
   }
 </script>
