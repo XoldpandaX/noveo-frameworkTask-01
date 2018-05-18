c43b785ba8fe698ebb749bebdbdba0dd9834e43b
XoldpandaX <rembo-87@yandex.ru>
2018-05-18 12:36:04 +0700

    added styles

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .card
+    .card__content
+      h2 Its Title
+      p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt eos facere iure laboriosam nam non odit vitae. Aliquam expedita necessitatibus ratione temporibus? Dolores, necessitatibus, quam.
+    footer
+      .edit-btn
+      .like-btn
+</template>
+
+<script>
+  export default {
+    name: 'card'
+  };
+</script>
--- src/containers/CardList.vue
+++ src/containers/CardList.vue
@@ -4,16 +4,19 @@
     .card-list__cards-container
       .card-list__element
         add-card-btn
-      .card-list__element(v-for="el in 7") {{ el }}
+      .card-list__element(v-for="el in 7")
+        card
 </template>
 
 <script>
   import AddCardBtn from './../components/AddCardBtn.vue';
+  import Card from './../components/Card.vue';
 
   export default {
     name: 'card-list',
     components: {
-      AddCardBtn
+      AddCardBtn,
+      Card
     }
-  }
+  };
 </script>
