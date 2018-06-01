fd4f07f0f9a3c16ecec3cdf5190a43ab91f5196f
Denis <Denis.Shakin@noveogroup.com>
2018-06-01 19:54:18 +0700

    edited code style

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -17,23 +17,27 @@
           button(@click.prevent="formDataToStore") save
 </template>
 <script>
-  import {mapGetters, mapActions} from 'vuex';
-  import {getRandomID} from './../../helpers/helpFunctions.js';
+  import { mapGetters, mapActions } from 'vuex';
+  import { getRandomID } from './../../helpers/helpFunctions.js';
 
   export default {
     name: 'card',
+
     props: {
       transformComponent: {
         type: String,
         default: 'text-card'
       },
+
       cardData: {
         type: Object
       },
+
       editData: {
         type: Object
       }
     },
+
     data() {
       return {
         maxSymbols: 196,
@@ -43,6 +47,7 @@
         }
       };
     },
+
     computed: {
       ...mapGetters('cards', ['cards']),
 
@@ -54,12 +59,13 @@
             return 'form-card';
         }
       },
+
       symbolsLeft() {
         return this.userInput.description.length;
       }
     },
     methods: {
-      ...mapActions('cards', ['addCardDataToStore', 'likeToggle', 'deleteCardDataFromStore', 'cardsDataInit']),
+      ...mapActions('cards', ['addCardDataToStore', 'deleteCardDataFromStore', 'cardsDataInit']),
 
       formDataToStore() {
         const {title, description} = this.userInput;
@@ -83,6 +89,7 @@
           alert('Enter some text');
         }
       },
+
       displayEditData() {
         if (this.editData) {
           const {title, description} = this.editData;
@@ -90,22 +97,22 @@
           this.userInput.description = description;
         }
       },
+
       deleteCard() {
         this.deleteCardDataFromStore(this.editData.id);
         this.redirectToHome();
       },
-      sendCardID() {
-        const cardId = this.cardData.id;
-        this.likeToggle(cardId);
-      },
+
       redirectToHome() {
         this.$router.push({name: 'home'});
       },
+
       redirectToEditCard() {
         const id = this.cardData.id.split('').splice(1).join('');
         this.$router.push({path: `/edit-card/${id}`})
       }
     },
+
     created() {
       this.displayEditData();
     }
