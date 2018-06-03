9f8f4413ea4cabe85f0dcbfaa6465e552830bdcf
XoldpandaX <rembo-87@yandex.ru>
2018-06-03 16:54:14 +0700

    added sort function

--- src/helpers/helpFunctions.js
+++ src/helpers/helpFunctions.js
@@ -1,3 +1,24 @@
 export function getRandomID() {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
+
+function sortByObjValue(key, order='asc') {
+  return function(a, b) {
+    if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      return 0;
+    }
+    
+    const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+    
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return ((order === 'desc') ? (comparison * -1) : comparison);
+  };
+}
+
+console.log(bands.sort(compareValues('albums')));
