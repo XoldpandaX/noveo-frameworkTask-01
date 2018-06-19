024ef92dbcf667230f524df74e5212793da7226c
Denis <Denis.Shakin@noveogroup.com>
2018-06-19 18:31:32 +0700

    added new components and styles for sign-in route

---
+++ src/views/SignIn.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  section.sign-in.page
+    .container
+      h1 Please Sign In
+      .sign-in__form-container
+        sign-in-form
+</template>
+
+<script>
+  import SignInForm from '../components/Forms/sign-in-form/SignInFrom.vue'
+
+  export default {
+    name: 'sign-in',
+
+    components: {
+      SignInForm
+    }
+  }
+</script>
