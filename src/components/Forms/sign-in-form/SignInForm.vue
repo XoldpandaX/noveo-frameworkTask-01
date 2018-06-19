60971b2d02705b5e0874095a60829c043a017390
Denis <Denis.Shakin@noveogroup.com>
2018-06-19 19:48:01 +0700

    added new styles && added base logic for form component

--- src/components/Forms/sign-in-form/SignInForm.vue
+++ src/components/Forms/sign-in-form/SignInForm.vue
@@ -1,20 +1,41 @@
 <template lang="pug">
   form.form-wrapper.-sign-in
-    h3 sign-in
+    h3 write in fields
     input(name="email",
           type="email",
-          placeholder="Email")
+          placeholder="Email",
+          autocomplete="foo")
     input(name="password",
           type="password",
-          placeholder="Password")
+          placeholder="Password",
+          autocomplete="foo")
     input(name="confirm-password",
           type="password",
-          placeholder="Confirm Password")
-
+          placeholder="Confirm Password",
+          autocomplete="foo",
+          class="-margin-bottom-xl")
+    button(class="form-wrapper__button -center",
+           @click.prevent="run()") Sign In
 </template>
 
 <script>
   export default {
-    name: 'sign-in-form'
+    name: 'sign-in-form',
+
+    data() {
+      return {
+        fildsData: {
+          email: '',
+          password: '',
+          confirmPassword: ''
+        }
+      };
+    },
+
+    methods: {
+      chickInputData() {
+
+      }
+    }
   }
 </script>
