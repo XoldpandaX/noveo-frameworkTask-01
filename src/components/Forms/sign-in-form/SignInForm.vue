ebd77827a4a81bf92ed3cbc1f689fd04e26a6499
XoldpandaX <rembo-87@yandex.ru>
2018-06-20 20:34:34 +0700

    created base check methods

--- src/components/Forms/sign-in-form/SignInForm.vue
+++ src/components/Forms/sign-in-form/SignInForm.vue
@@ -1,22 +1,33 @@
 <template lang="pug">
   form.form-wrapper.-sign-in
     h3 write-in fields
-    input(name="email",
-          type="email",
-          placeholder="Email",
-          autocomplete="foo",
-          v-model="email")
-    input(name="password",
-          type="password",
-          placeholder="Password",
-          autocomplete="foo",
-          v-model="password")
-    input(class="-margin-bottom-xl",
-          name="confirm-password",
-          type="password",
-          placeholder="Confirm Password",
-          autocomplete="foo",
-          v-model="confirmPassword")
+    .form-wrapper__field
+      input(name="email",
+            type="email",
+            placeholder="Email",
+            autocomplete="foo",
+            v-model="email")
+      .form-wrapper__error(v-if="isMailValid")
+        span sss
+
+    .form-wrapper__field
+      input(name="password",
+            type="password",
+            placeholder="Password",
+            autocomplete="foo",
+            v-model="password")
+      .form-wrapper__error
+        span sss
+
+    .form-wrapper__field.-margin-bottom-xl
+      input(name="confirm-password",
+            type="password",
+            placeholder="Confirm Password",
+            autocomplete="foo",
+            v-model="confirmPassword")
+      .form-wrapper__error
+        span sss
+
     button.form-wrapper__button.-margin-block-center(@click.prevent="fieldsDataHandler()") Sign In
 </template>
 
@@ -33,7 +44,17 @@
     },
 
     computed: {
+      isMailValid() {
 
+      },
+
+      isPasswordValid() {
+
+      },
+
+      isPasswordsAreEqual() {
+
+      }
     },
 
     methods: {
