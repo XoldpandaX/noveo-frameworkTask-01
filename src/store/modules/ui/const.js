60a828b7a3ad897b8f1585afeca68df77a294a1b
XoldpandaX <rembo-87@yandex.ru>
2018-06-20 22:20:33 +0700

    change default state

--- src/store/modules/ui/const.js
+++ src/store/modules/ui/const.js
@@ -1,23 +1,6 @@
-const navigationElements = {
-  defaultNav: [
-    {
-      id: 0,
-      isActive: false,
-      title: 'sign up',
-      route: '/sign-up',
-      iconClass: '-violet'
-    },
-    {
-      id: 1,
-      isActive: false,
-      title: 'sign in',
-      route: '/sign-in',
-      iconClass: '-orange'
-    }
-  ]
-};
+import constants from '../../../constants';
 
 export const defaultState = {
   modals: [],
-  navigationBar: navigationElements.defaultNav
+  navigationBar: constants.navigationElements.defaultNav
 };
