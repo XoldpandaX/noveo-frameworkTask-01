50438647d2516265b75c898c38729ed8807b30d5
XoldpandaX <rembo-87@yandex.ru>
2018-06-23 11:47:55 +0700

    markup changes

---
+++ src/components/Cards/CardButtonAdd.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  app-button.-round.-no-background-color.-plus
+</template>
+
+<script>
+  import AppButton from '../Buttons/AppButton.vue';
+
+  export default {
+    name: "CardButtonAdd",
+
+    components: {
+      AppButton
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   section.home.page
     h1 Card List
     container-card-list
+    CardButtonAdd
 </template>
 
 <script>
   import ContainerCardList from '../containers/CardContainers/ContainerCardList.vue';
+  import CardButtonAdd from '../components/Cards/CardButtonAdd';
 
   export default {
     name: 'home',
 
     components: {
-      ContainerCardList
+      ContainerCardList,
+      CardButtonAdd
     }
   }
 </script>
