79a7470a94b8cd05046fb4f7cf08aa915b1dcfda
XoldpandaX <rembo-87@yandex.ru>
2018-06-23 13:28:40 +0700

    added main layout

---
+++ src/components/Base/MainLayout.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  main.main-content
+    .layout-container
+      transition(name="router-animation",
+                 enter-active-class="animated fadeInLeft",
+                 leave-active-class="animated fadeOutRight")
+        router-view
+</template>
+
+<script>
+  export default {
+    name: "MainLayout"
+  }
+</script>
