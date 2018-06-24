71be73c5dcd8882b4782b2a595c7ffe874da1acf
XoldpandaX <rembo-87@yandex.ru>
2018-06-24 18:17:45 +0700

    added new components

---
+++ src/components/Forms/FormNewCard/FormButtonSave.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  app-button(:propButtonType="buttonAppearence") save
+</template>
+
+<script>
+  import AppButton from '../../AppButton.vue';
+
+  export default {
+    name: 'FormButtonSave',
+
+    components: {
+      AppButton
+    },
+
+    data() {
+      return {
+        buttonAppearence: 'formButton'
+      };
+    }
+  }
+</script>
---
+++ src/components/Forms/FormNewCard/FormNewCard.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  form.new-card
+    .new-card__row
+      input(type="text",
+            placeholder="Enter title",
+            maxlength="42")
+    .new-card__row
+      textarea(placeholder="Enter description",
+               maxlength="maxSymbols")
+      span.new-card__counter 0/196
+    .app-button__row
+      FormButtonSave
+</template>
+<script>
+  import FormButtonSave from './FormButtonSave.vue';
+
+  export default {
+    name: 'FormNewCard',
+
+    components: {
+      FormButtonSave
+    }
+  }
+</script>
