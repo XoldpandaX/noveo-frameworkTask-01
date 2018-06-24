5ae143d3f5e7c90f06d963e137f093a6d4a1faab
XoldpandaX <rembo-87@yandex.ru>
2018-06-24 16:36:13 +0700

    changed old card list to new

---
+++ src/components/Cards/CardList.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .card-list
+    .card-list__cards-container
+      .card-list__element
+        card-button-add
+      .card-list__element(v-for="card in cards")
+        card-info(:cardData="card",
+                  :key="card.id")
+</template>
+
+<script>
+  import CardButtonAdd from './CardButtonAdd.vue';
+  import CardInfo from './CardInfo.vue';
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'CardList',
+
+    components: {
+      CardButtonAdd,
+      CardInfo
+    },
+
+    computed: {
+      ...mapGetters('cards', ['cards'])
+    }
+  };
+</script>
