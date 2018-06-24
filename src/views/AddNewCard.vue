614e97c184ffab798ecd993fa1144cad559984e4
XoldpandaX <rembo-87@yandex.ru>
2018-06-24 18:16:56 +0700

    imported new component

--- src/views/AddNewCard.vue
+++ src/views/AddNewCard.vue
@@ -3,16 +3,19 @@
     h1.page__title Add New Card
     .add-new-card__row
       card-form
+    form-new-card
 </template>
 
 <script>
   import CardForm from '../containers/CardContainers/ContainerCardForm.vue';
+  import FormNewCard from '../components/Forms/FormNewCard/FormNewCard.vue';
 
   export default {
     name: 'add-new-card',
 
     components: {
-      CardForm
+      CardForm,
+      FormNewCard
     }
   }
 </script>
