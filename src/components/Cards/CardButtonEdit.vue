72a2cb7f26cbe4ff636c1bf21326ea5d77bd6536
XoldpandaX <rembo-87@yandex.ru>
2018-06-24 15:42:32 +0700

    added new component

---
+++ src/components/Cards/CardButtonEdit.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  AppButton(:onClick="redirectToEditPage",
+            :propButtonType="buttonAppearence")
+</template>
+
+<script>
+  import AppButton from '../AppButton.vue';
+
+  export default {
+    name: 'CardButtonEdit',
+
+    components: {
+      AppButton
+    },
+
+    props: {
+      cardID: {
+        type: String,
+        required: true
+      }
+    },
+
+    data() {
+      return {
+        buttonAppearence: 'editButton'
+      };
+    },
+
+    methods: {
+      redirectToEditPage() {
+        const id = this.cardID.split('').splice(1).join('');
+        this.$router.push({path: `/edit-card/${id}`})
+      }
+    }
+  }
+</script>
