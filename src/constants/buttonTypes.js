516500c0c4da953f09e803e71aac6ec118033fce
XoldpandaX <rembo-87@yandex.ru>
2018-06-24 15:42:54 +0700

    added new type of button

--- src/constants/buttonTypes.js
+++ src/constants/buttonTypes.js
@@ -1,6 +1,10 @@
 export default [
   {
     type: 'roundWithPlusIcon',
-    classes: ['-round', '-no-background-color', '-plus']
+    classes: ['-round', '-no-background-color', '-plus-btn']
+  },
+  {
+    type: 'editButton',
+    classes: ['-edit-btn', '-no-background-color', '-no-padding']
   }
 ];
