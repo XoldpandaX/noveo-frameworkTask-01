abeb11cf538e81a9c09c37b9d6695027b113a838
XoldpandaX <rembo-87@yandex.ru>
2018-06-24 16:27:25 +0700

    added new methods

---
+++ src/components/Cards/CardButtonLike.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  app-button(:onClick="likeCard",
+             :propButtonType="buttonAppearence",
+             :size="buttonSize",
+             :class="likeIconActive")
+</template>
+
+<script>
+  import AppButton from '../AppButton.vue';
+  import { mapActions } from 'vuex';
+
+  export default {
+    name: 'CardButtonLike',
+
+    components: {
+      AppButton
+    },
+
+    props: {
+      cardID: {
+        type: String,
+        required: true
+      },
+
+      isCardLike: {
+        type: Boolean,
+        required: true
+      }
+    },
+
+    data() {
+      return {
+        buttonAppearence: 'likeButton',
+        buttonSize: {
+          width: '25px',
+          height: '25px'
+        }
+      };
+    },
+
+    computed: {
+      likeIconActive() {
+        return this.isCardLike ? '-active' : '';
+      }
+    },
+
+    methods: {
+      ...mapActions('cards', ['likeToggle']),
+
+      likeCard() {
+        this.likeToggle(this.cardID);
+      }
+    }
+  }
+</script>
