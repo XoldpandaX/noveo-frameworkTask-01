8a115d5191b39df202be8840c56a204bc076a112
XoldpandaX <rembo-87@yandex.ru>
2018-06-30 19:50:22 +0700

    little fixies in button

---
+++ src/components/Buttons/ButtonCardDelete.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  app-button(:onClick="deleteCard",
+             :propButtonType="buttonAppearence") Delete
+</template>
+
+<script>
+  import AppButton from '../AppButton.vue';
+  import { mapActions } from 'vuex';
+
+  export default {
+    name: 'ButtonCardDelete',
+
+    components: {
+      AppButton
+    },
+
+    props: {
+      cardID: {
+        type: String,
+        required: true
+      }
+    },
+
+    data() {
+      return {
+        buttonAppearence: 'formButton'
+      };
+    },
+
+    methods: {
+      ...mapActions('cards', ['deleteCardDataFromStore']),
+
+      deleteCard() {
+        this.deleteCardDataFromStore(this.cardID);
+        this.$router.push({name: 'home'});
+      },
+    }
+  }
+</script>
