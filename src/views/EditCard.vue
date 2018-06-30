6790c77039ac2742d43269cded8e50c4e5348414
XoldpandaX <rembo-87@yandex.ru>
2018-06-30 20:03:02 +0700

    removed excess code

--- src/views/EditCard.vue
+++ src/views/EditCard.vue
@@ -2,13 +2,11 @@
   section.edit-card.page(v-if="showPage")
     h1.page__title Edit Selected Card Data
     .edit-card__row
-      container-card-edit(:data="cardData")
-      FormAddChangeCard(:cardData="cardData", transform="edit-form")
+      form-add-change-card(:cardData="cardData", transform="edit-form")
 </template>
 
 <script>
-  import { mapGetters, mapActions } from 'vuex';
-  import ContainerCardEdit from '../containers/CardContainers/ContainerCardEdit.vue';
+  import { mapGetters } from 'vuex';
   import FormAddChangeCard from '../components/Forms/CardForm/FormAddChangeCard.vue'
 
   export default {
@@ -17,37 +15,25 @@
     props: {
       id: {
         type: String
-      },
-      name: {
-        type: String
       }
     },
 
     components: {
-      ContainerCardEdit,
       FormAddChangeCard
     },
 
     data() {
       return {
         showPage: false,
-        cardData: {
-          id: '',
-          order: '',
-          title: '',
-          description: '',
-          like: false
-        }
+        cardData: {}
       };
     },
 
     computed: {
-      ...mapGetters('cards', ['cards', 'getCardByID'])
+      ...mapGetters('cards', ['getCardByID'])
     },
 
     methods: {
-      ...mapActions('cards', ['searchCardsById']),
-
       redirectTo() {
         this.$router.push({path: '/'});
       },
@@ -56,13 +42,10 @@
         const card = this.getCardByID(`#${this.id}`);
 
         if (card) {
-          let { id, title, description, like, order } = card;
+          this.cardData = {
+            ...card
+          };
 
-          this.cardData.id = id;
-          this.cardData.order = order;
-          this.cardData.title = title;
-          this.cardData.description = description;
-          this.cardData.like = like;
           this.showPage = true;
           return;
         }
