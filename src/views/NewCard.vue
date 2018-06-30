65d6cfa8d8dec1bd694dfe037c97651d9ef2088a
XoldpandaX <rembo-87@yandex.ru>
2018-06-30 12:51:04 +0700

    renamed component

---
+++ src/views/NewCard.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  section.add-new-card.page
+    h1.page__title Add New Card
+    .add-new-card__row
+      form-add-change-card
+</template>
+
+<script>
+  import FormAddChangeCard from '../components/Forms/CardForm/FormAddChangeCard.vue';
+
+  export default {
+    name: 'AddNewCard',
+
+    components: {
+      FormAddChangeCard
+    }
+  }
+</script>
