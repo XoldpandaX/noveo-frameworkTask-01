a38c49e3ee64dc84ab78d1906dd4c183430d51e8
XoldpandaX <rembo-87@yandex.ru>
2018-06-30 19:47:23 +0700

    changed import components

--- src/components/Cards/CardInfo.vue
+++ src/components/Cards/CardInfo.vue
@@ -4,21 +4,21 @@
       h2 {{ cardData.title }}
       p {{ cardData.description }}
     footer
-      card-button-edit(:cardID="cardData.id")
-      card-button-like(:cardID="cardData.id",
+      button-card-edit(:cardID="cardData.id")
+      button-card-like(:cardID="cardData.id",
                        :isCardLike="cardData.like")
 </template>
 
 <script>
-  import CardButtonEdit from '../../components/Cards/CardButtonEdit.vue';
-  import CardButtonLike from '../../components/Cards/CardButtonLike.vue';
+  import ButtonCardEdit from '../Buttons/ButtonCardEdit.vue';
+  import ButtonCardLike from '../Buttons/ButtonCardLike.vue';
 
   export default {
     name: 'cardInfo',
 
     components: {
-      CardButtonEdit,
-      CardButtonLike
+      ButtonCardEdit,
+      ButtonCardLike
     },
 
     props: {
