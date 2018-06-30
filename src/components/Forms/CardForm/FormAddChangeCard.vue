493da76164e10d98457f15d080f205d44e8bb534
XoldpandaX <rembo-87@yandex.ru>
2018-06-30 19:49:00 +0700

    completed form functional from CardForm

--- src/components/Forms/CardForm/FormAddChangeCard.vue
+++ src/components/Forms/CardForm/FormAddChangeCard.vue
@@ -11,18 +11,32 @@
                v-model="userInput.description")
       span.new-card__counter {{ symbolsLeft }}/{{ maxSymbols }}
     .app-button__row
-      FormButtonSave
+      button-card-delete(v-if="transform === 'edit-form'",
+                           :cardID="cardData.id") Delete
+      button-card-save(:formDataToStore="formDataToStore") Save
 </template>
 <script>
-  import FormButtonSave from './FormButtonSave.vue';
   import { mapGetters, mapActions } from 'vuex';
   import { getRandomID } from '../../../helpers/index.js';
+  import ButtonCardSave from '../../Buttons/ButtonCardSave.vue';
+  import ButtonCardDelete from '../../Buttons/ButtonCardDelete.vue';
 
   export default {
-    name: 'FormNewCard',
+    name: 'FormAddChangeCard',
 
     components: {
-      FormButtonSave
+      ButtonCardSave,
+      ButtonCardDelete
+    },
+
+    props: {
+      cardData: {
+        type: Object
+      },
+
+      transform: {
+        type: String
+      },
     },
 
     data() {
@@ -36,9 +50,45 @@
     },
 
     computed: {
+      ...mapGetters('cards', ['cards']),
+
       symbolsLeft() {
         return this.userInput.description.length;
       }
+    },
+
+    methods: {
+      ...mapActions('cards', ['addCardDataToStore', 'cardsDataInit']),
+
+      formDataToStore() {
+        const { title, description } = this.userInput;
+
+        if (title !== '' && description !== '') {
+          const storeData = {
+            id: this.cardData ? this.cardData.id : `#${getRandomID()}`,
+            order: this.cardData ? this.cardData.order : this.cards.length,
+            title: title,
+            description: description,
+            like: this.cardData ? this.cardData.like : false
+          };
+
+          this.addCardDataToStore(storeData);
+          this.$router.push({name: 'home'});
+          this.cardsDataInit();
+        } else {
+          alert('Enter some text');
+        }
+      },
+
+      displayEditData() {
+        const { title, description } = this.cardData;
+        this.userInput.title = title;
+        this.userInput.description = description;
+      }
+    },
+
+    created() {
+      if (this.cardData) this.displayEditData();
     }
   }
 </script>
