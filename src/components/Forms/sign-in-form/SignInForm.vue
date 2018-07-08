5fd8b7ab1da7efb6205e7d43d992b635c449d4b8
XoldpandaX <rembo-87@yandex.ru>
2018-07-08 09:16:15 +0700

    restrcuried component

--- src/components/Forms/sign-in-form/SignInForm.vue
+++ src/components/Forms/sign-in-form/SignInForm.vue
@@ -9,8 +9,8 @@
             v-model="email")
       transition(enter-active-class="animated bounceIn",
                  leave-active-class="animated fadeOut")
-        .form-wrapper__error(v-if="isEmailValid")
-          span {{ errorMsg.emailIsIncorrect }}
+        .form-wrapper__error(v-if="errors.email.error")
+          span {{ errors.email.errorMessage }}
 
     .form-wrapper__field
       input(name="password",
@@ -20,8 +20,8 @@
             v-model="password")
       transition(enter-active-class="animated bounceIn",
       leave-active-class="animated fadeOut")
-        .form-wrapper__error(v-if="isPasswordValid")
-          span {{ errorMsg.passwordIsIncorrect }}
+        .form-wrapper__error(v-if="errors.password.error")
+          span {{ errors.password.errorMessage }}
 
     .form-wrapper__field.-margin-bottom-xl
       input(name="confirm-password",
@@ -31,31 +31,62 @@
             v-model="confirmPassword")
       transition(enter-active-class="animated bounceIn",
       leave-active-class="animated fadeOut")
-        .form-wrapper__error(v-if="isPasswordsAreEqual")
-          span {{ errorMsg.confirmPasswordIsIncorrect }}
-    button.form-wrapper__button.-margin-block-center(@click.prevent="fieldsDataHandler()") Sign In
+        .form-wrapper__error(v-if="errors.confirmPassword.error")
+          span {{ errors.confirmPassword.errorMessage }}
+    .app-button__row
+      app-button(propButtonType="formButton", :onClick="checkEmail") Sign Up
 </template>
 
 <script>
+  import AppButton from '../../../components/AppButton.vue';
 
   export default {
     name: 'SignInForm',
+
+    components: {
+      AppButton
+    },
 
     data() {
       return {
         email: '',
         password: '',
         confirmPassword: '',
-        errorMsg: {
-          emailIsIncorrect: '',
-          passwordIsIncorrect: '',
-          confirmPasswordIsIncorrect: ''
+        errors: {
+          email: {
+            errorMessage: '',
+            error: false
+          },
+          password: {
+            errorMessage: '',
+            error: false
+          },
+          confirmPassword: {
+            errorMessage: '',
+            error: false
+          }
         }
       };
     },
 
-    methods: {
+    computed: {
+      isEmailValid() {
+        return true;
+      },
 
+      isPasswordValid() {
+        return true;
+      },
+
+      isPasswordsAreEqual() {
+        return true;
+      }
+    },
+
+    methods: {
+      checkEmail(mail) {
+        console.log('name');
+      }
     }
   };
 </script>
