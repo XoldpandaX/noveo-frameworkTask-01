07005631af165b1ed5175fb1c8cf7d8b21feb76e
XoldpandaX <rembo-87@yandex.ru>
2018-07-08 12:21:05 +0700

    added filters && added functions to register filters globally

---
+++ src/filters/index.js
@@ -0,0 +1,5 @@
+import * as text from './text.js';
+
+export default {
+  ...text
+};
---
+++ src/filters/text.js
@@ -0,0 +1,5 @@
+export function makeUppercase(val) {
+  if (!val) return '';
+  val = val.toString();
+  return val.toUpperCase();
+}
