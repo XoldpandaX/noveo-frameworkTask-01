121c28962235d3445e4e262d708452805a353bb8
Denis <Denis.Shakin@noveogroup.com>
2018-07-11 09:41:56 +0700

    added new constant

---
+++ src/constants/forms.js
@@ -0,0 +1,34 @@
+export default {
+  signInForm: {
+    inputs: [
+      {
+        name: 'name',
+        type: 'text',
+        placeholder: 'Name',
+        autocomplete: 'foo',
+        value: ''
+      },
+      {
+        name: 'email',
+        type: 'email',
+        placeholder: 'Email',
+        autocomplete: 'foo',
+        value: ''
+      },
+      {
+        name: 'password',
+        type: 'password',
+        placeholder: 'Password',
+        autocomplete: 'foo',
+        value: ''
+      },
+      {
+        name: 'confirmPassword',
+        type: 'password',
+        placeholder: 'Confirm Password',
+        autocomplete: 'foo',
+        value: ''
+      }
+    ]
+  }
+}
