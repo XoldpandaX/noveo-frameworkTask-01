6251e2cc7cbc2ba1ec0d539bfbf119cd6cff733c
Denis <Denis.Shakin@noveogroup.com>
2018-07-11 09:42:14 +0700

    added new constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,11 @@
 import navigationElements from './navigationElements.js';
 import buttonTypes from './buttonTypes.js';
 import regularExpressions from './regularExpressions.js';
+import forms from './forms.js';
 
 export default {
   navigationElements,
   buttonTypes,
-  regularExpressions
+  regularExpressions,
+  forms
 }
