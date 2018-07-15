3523292e1e54e52ee5c1f44c6dae61c050881a86
XoldpandaX <rembo-87@yandex.ru>
2018-07-15 13:27:23 +0700

    edited constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,10 +2,12 @@ import navigationElements from './navigationElements.js';
 import buttonTypes from './buttonTypes.js';
 import regularExpressions from './regularExpressions.js';
 import forms from './forms.js';
+import apiMap from './api-map.js';
 
 export default {
   navigationElements,
   buttonTypes,
   regularExpressions,
-  forms
+  forms,
+  apiMap
 }
