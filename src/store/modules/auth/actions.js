e74166b75c00bbed11cd7822fe7f1d8ec00166ac
XoldpandaX <rembo-87@yandex.ru>
2018-07-15 15:33:01 +0700

    added new module && crated first action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,7 +1,15 @@
 import * as types from './mutation-types.js';
+import auth from '../../../api/auth.requests.js'
 
-
+async function registerUser({ commit }, userData) {
+  try {
+    const { data } = await auth.registerUser(userData);
+    console.log(data);
+  } catch(err) {
+    console.log(err);
+  }
+}
 
 export default {
-
+  registerUser
 };
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,9 +1,11 @@
+import auth from './auth';
 import cards from './cards';
 import localStorage from './localStorage';
 import ui from './ui';
 
 export default {
+  auth,
   cards,
   localStorage,
   ui
-}
+};
