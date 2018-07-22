860c0d5228032eefd13d0de399b7e8251fbc98ac
XoldpandaX <rembo-87@yandex.ru>
2018-07-22 20:49:58 +0700

    changed local storage mutation

--- src/store/modules/localStorage/mutations.js
+++ src/store/modules/localStorage/mutations.js
@@ -1,8 +1,17 @@
 import * as types from './mutation-types.js';
+import _ from 'lodash/lang';
 
 export default {
   [types.ADD_LOCAL_STORAGE_DATA] (state, data) {
-    localStorage.setItem(data.id, JSON.stringify(data));
+    //localStorage.setItem(data.id, JSON.stringify(data));
+    //localStorage.setItem('cards', JSON.stringify(arr));
+    const currentLocalStorage = JSON.parse(localStorage.getItem('cards'));
+    if (_.isNull(currentLocalStorage)) {
+      localStorage.setItem('cards', JSON.stringify([data]));
+    } else {
+      currentLocalStorage.push(data);
+      localStorage.setItem('cards', JSON.stringify(currentLocalStorage));
+    }
   },
   
   [types.DELETE_LOCAL_STORAGE_DATA] (state, cardID) {
