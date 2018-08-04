84a001915a35ef4852b4c9b8843cd8f2b75a7c2c
XoldpandaX <rembo-87@yandex.ru>
2018-08-04 18:02:10 +0700

    changed service

--- src/services/localStorageProvider.js
+++ src/services/localStorageProvider.js
@@ -18,25 +18,27 @@ class LocalStorageProvider {
   setItem(key, val) {
     this[`_${key}`] = val;
     localStorage.setItem(key, val);
-    
     return this;
   }
   
+  setStringifyItem(key, val) {
+    const stringifyVal = JSON.stringify(val);
+    this[`_${key}`] = stringifyVal;
+    localStorage.setItem(key, stringifyVal);
+  }
+  
   setObjItemInArray(key, val) {
-    let currData = isUndefined(this[`_${key}`]) ? [] : JSON.parse(this[`_${key}`]);
-    const itemId = val.id;
-    const itemIndex = this._findItemIndexById(currData, itemId);
+    let currData = isUndefined(this[`_${key}`]) ? [] : this.getParseItem(key);
+    const itemIndex = this._findItemIndexById(currData, val.id);
     
     switch (itemIndex) {
-      case itemIndex >= 0:
-        currData.splice(itemIndex, 1, val);
+      case -1:
+        currData.push(val);
         break;
       default:
-        currData.push(val);
+        currData.splice(itemIndex, 1, val);
     }
-  
-    currData = JSON.stringify(currData);
-    this.setItem(key, currData);
+    this.setStringifyItem(key, currData);
   }
   
   removeItem(key) {
@@ -44,9 +46,17 @@ class LocalStorageProvider {
     localStorage.removeItem(key);
   }
   
+  removeObjItemFromArray(key, id) {
+    let currData = this.getParseItem(key);
+    const itemIndex = this._findItemIndexById(currData, id);
+    
+    currData.splice(itemIndex, 1);
+    this.setStringifyItem(key, currData);
+  }
+  
   // PRIVATE METHODS
   _findItemIndexById(searchArray, id) {
-    return id ? searchArray.findIndex(el => el.id === id) : -1;
+    return searchArray.findIndex(el => el.id === id);
   }
 }
 
