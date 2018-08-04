6ff53a371c8d466f5d2c2e80d730035dd6b6db49
XoldpandaX <rembo-87@yandex.ru>
2018-08-04 18:25:26 +0700

    removed excess store module

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,11 +1,9 @@
 import auth from './auth';
 import cards from './cards';
-import localStorage from './localStorage';
 import ui from './ui';
 
 export default {
   auth,
   cards,
-  localStorage,
   ui
 };
