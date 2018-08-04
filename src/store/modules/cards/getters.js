d9f4408830b69b3fca23f79850f5087e5d95391c
XoldpandaX <rembo-87@yandex.ru>
2018-08-04 18:51:40 +0700

    added new getter

--- src/store/modules/cards/getters.js
+++ src/store/modules/cards/getters.js
@@ -1,6 +1,11 @@
 export default {
   cards: state => state.cards,
+  
   getCardByID: (state) => (id) => {
     return state.cards.find(card => card.id === id);
+  },
+  
+  getCardIndexByID: (state) => (id) => {
+    return state.cards.findIndex(el => el.id === id);
   }
 }
