b652c239add852780f6151f0fbb4c67fca5c5612
XoldpandaX <rembo-87@yandex.ru>
2018-08-05 14:20:09 +0700

    removed uneused functions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,25 +2,6 @@ export function getRandomID() {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
 
-export function sortByObjValue(key, order='asc') {
-  return function(a, b) {
-    if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-      return 0;
-    }
-    
-    const varA = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
-    const varB = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
-    
-    let comparison = 0;
-    if (varA > varB) {
-      comparison = 1;
-    } else if (varA < varB) {
-      comparison = -1;
-    }
-    return ((order === 'desc') ? (comparison * -1) : comparison);
-  };
-}
-
 export function checkObjectFieldsForTrueValue(obj) {
   let falseResult = 0;
   
@@ -39,10 +20,6 @@ export function objFieldByValue(searchObj, searchField , value) {
 
 export function isObjFieldsAreEmpty(arrOfFields, fieldForCheck) {
   return arrOfFields.some(val => val[fieldForCheck] === '');
-}
-
-export function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 export function capitaliseFirstLetter(string) {
