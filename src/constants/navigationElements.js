1841c5dca08efa9effc5c1b0489ce19be2bc5253
XoldpandaX <rembo-87@yandex.ru>
2018-08-11 20:04:51 +0700

    logged in user navigatiion

--- src/constants/navigationElements.js
+++ src/constants/navigationElements.js
@@ -2,16 +2,27 @@ export default {
   defaultNav: [
     {
       id: 0,
-      isActive: false,
       title: 'sign up',
       route: '/sign-up',
       iconClass: '-violet'
     },
     {
       id: 1,
-      isActive: false,
       title: 'sign in',
       route: '/sign-in',
+      iconClass: '-orange'
+    }
+  ],
+  userNav: [
+    {
+      id: 0,
+      title: 'my profile',
+      iconClass: '-violet'
+    },
+    {
+      id: 1,
+      title: 'logout',
+      action: 'exit',
       iconClass: '-orange'
     }
   ]
