903088e1c8649a20a2f5d86f462256aefcd3eb5c
XoldpandaX <rembo-87@yandex.ru>
2018-08-11 17:43:12 +0700

    authorized status changer

--- src/store/modules/auth/mutations.js
+++ src/store/modules/auth/mutations.js
@@ -3,6 +3,7 @@ import * as types from './mutation-types';
 export default {
   [types.HANDLE_LOGIN] (state, token) {
     state.token = token;
+    state.status = 'authorized';
   },
   
   [types.SAVE_USER_DATA] (state, userData) {
