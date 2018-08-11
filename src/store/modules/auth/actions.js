cb027c3b2e98fb9cb3e0ee8d2e5a48b74f8a8cd2
XoldpandaX <rembo-87@yandex.ru>
2018-08-11 12:51:25 +0700

    added new action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,6 @@
 import * as types from './mutation-types.js';
-import auth from '../../../api/auth.requests.js'
+import auth from '../../../api/auth.requests.js';
+import LocalStorageProvider from '../../../services/localStorageProvider.js';
 
 async function registerUser({ commit }, userData) {
   try {
@@ -10,6 +11,21 @@ async function registerUser({ commit }, userData) {
   }
 }
 
+async function loginUser({ commit }, userData) {
+  try {
+    const { data } = await auth.loginUser(userData);
+    const { token } = data.data;
+    LocalStorageProvider.setItem('token', token);
+    
+    return true;
+  } catch(err) {
+    LocalStorageProvider.removeItem('token');
+    console.log(err);
+    return false;
+  }
+}
+
 export default {
-  registerUser
+  registerUser,
+  loginUser
 };
