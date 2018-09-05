518c05b2886e928b36ce9789f093ea1bea03d658
XoldpandaX <rembo-87@yandex.ru>
2018-09-05 14:12:46 +0700

    removed restructuring of state data

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -2,14 +2,5 @@ export default {
   isAuthenticated: state => !!state.status,
   authStatus: state => state.status,
   userRole: state => state.user.role,
-  userData: state => {
-    const { email, id, name, role, created_at }  = state.user;
-    return {
-      id,
-      email,
-      name,
-      role,
-      created_at
-    }
-  }
+  userData: state => state.user
 }
