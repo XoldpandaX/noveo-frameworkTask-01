b6edd18a36b90924f045e99fa165c0f06af22669
XoldpandaX <rembo-87@yandex.ru>
2018-09-05 10:51:25 +0700

    import new modal

--- src/containers/ModalContainer/ContainerModalWrapper.vue
+++ src/containers/ModalContainer/ContainerModalWrapper.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .modal-wrapper(v-if="modals.length > 0")
+  .modal-wrapper(v-if="modals.length > 0", @click="closeModal")
     template(v-for="modal in modals")
       component(:is="modal.id",  @closeModal="closeModal")
 </template>
@@ -7,12 +7,14 @@
 <script>
   import { mapGetters, mapActions } from 'vuex';
   import FirstLoadModal from '../../components/Modals/FirstLoadModal.vue';
+  import ProfileModal from '../../components/Modals/ProfileModal.vue';
 
   export default {
     name: 'ContainerModalWrapper',
 
     components: {
-      FirstLoadModal
+      FirstLoadModal,
+      ProfileModal
     },
 
     computed: {
