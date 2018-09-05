17440f5ad5d0f54db37f02c005bc557cf0e4d478
XoldpandaX <rembo-87@yandex.ru>
2018-09-05 10:56:49 +0700

    updated getters

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,5 +1,15 @@
 export default {
   isAuthenticated: state => !!state.status,
   authStatus: state => state.status,
-  userRole: state => state.user.role
+  userRole: state => state.user.role,
+  userData: state => {
+    const { email, id, name, role, created_at: { date } } = state.user;
+    return {
+      id,
+      email,
+      name,
+      role,
+      date
+    }
+  }
 }
