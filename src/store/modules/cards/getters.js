f1312d432c291962b572e3c70e107b06022b23a6
XoldpandaX <rembo-87@yandex.ru>
2018-09-10 09:36:42 +0700

    little fixies

--- src/store/modules/cards/getters.js
+++ src/store/modules/cards/getters.js
@@ -1,5 +1,5 @@
 export default {
   cards: state => state.cards,
-  cardByID: state => id => state.cards.find(card => card.id === id),
+  cardByID: state => id => state.cards.find(card => card.id === Number(id)),
   cardIndexByID: state => id => state.cards.findIndex(el => el.id === id)
 }
