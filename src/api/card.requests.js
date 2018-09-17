07e9a3804bb87e8f515f9285cf9ea8568aadd340
XoldpandaX <rembo-87@yandex.ru>
2018-09-17 11:38:09 +0700

    added new card api

--- src/api/card.requests.js
+++ src/api/card.requests.js
@@ -10,13 +10,17 @@ export default {
     return Vue.axios.post(api.cards.createCard, cardData);
   },
   
-  removeCard(cardId) {
-    return Vue.axios.delete(api.cards.removeCard.replace('{id}', cardId));
-  },
-  
   editCard(data) {
     const { id, title, content } = data;
     console.log(id);
     return Vue.axios.put(api.cards.editCard.replace('{id}', id), { title, content });
+  },
+  
+  removeCard(cardId) {
+    return Vue.axios.delete(api.cards.removeCard.replace('{id}', cardId));
+  },
+  
+  toggleCardLike(cardId) {
+    return Vue.axios.post(api.cards.likeToggle.replace('{id}', cardId));
   }
 }
