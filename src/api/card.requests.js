a1f909f5857d9edbb431c8a542fbf09237db3497
XoldpandaX <rembo-87@yandex.ru>
2018-09-30 15:22:57 +0700

    change param

--- src/api/card.requests.js
+++ src/api/card.requests.js
@@ -2,8 +2,8 @@ import Vue from 'vue';
 import api from '../constants/api-map.js';
 
 export default {
-  getAllCards (liked, order_by, sort, per_page) {
-    return Vue.axios.get(api.cards.allCards, { params: { liked, order_by, sort, per_page } });
+  getAllCards (params) {
+    return Vue.axios.get(api.cards.allCards, { params: params });
   },
 
   createCard (cardData) {
