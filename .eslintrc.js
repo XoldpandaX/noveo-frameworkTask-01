c02a936947321473f61376aaa0b047a28c840247
XoldpandaX <rembo-87@yandex.ru>
2018-09-30 11:15:20 +0700

    changed settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,11 @@ module.exports = {
     es6: true
   },
   'extends': [
-    'plugin:vue/recommended',
-    '@vue/standard'
+    'eslint:recommended',
+    'plugin:vue/essential',
   ],
   rules: {
-    // 'no-console': process.env.NODE_ENV === 'production' ? 2 : 2,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 2,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 2,
     'semi': [2, 'always']
   }
