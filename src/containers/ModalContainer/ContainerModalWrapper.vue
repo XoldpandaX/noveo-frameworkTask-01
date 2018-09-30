d1dd447245ae9e0ed60736cbadea3f82ab30385d
XoldpandaX <rembo-87@yandex.ru>
2018-09-30 20:54:27 +0700

    imported new modal

--- src/containers/ModalContainer/ContainerModalWrapper.vue
+++ src/containers/ModalContainer/ContainerModalWrapper.vue
@@ -8,13 +8,15 @@
 import { mapGetters, mapActions } from 'vuex';
 import FirstLoadModal from '../../components/Modals/FirstLoadModal.vue';
 import ProfileModal from '../../components/Modals/ProfileModal.vue';
+import ConfirmModal from '../../components/Modals/ConfirmModal.vue';
 
 export default {
   name: 'ContainerModalWrapper',
 
   components: {
     FirstLoadModal,
-    ProfileModal
+    ProfileModal,
+    ConfirmModal
   },
 
   computed: {
