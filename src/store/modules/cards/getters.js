daaddb94711fc3d007ede93cb700bc8a861451d7
XoldpandaX <rembo-87@yandex.ru>
2018-10-27 17:41:51 +0700

    removed excess getter

--- src/store/modules/cards/getters.js
+++ src/store/modules/cards/getters.js
@@ -1,5 +1,4 @@
 export default {
   cards: state => state.cards,
-  cardByID: state => id => state.cards.find(card => card.id === Number(id)),
-  cardIndexByID: state => id => state.cards.findIndex(el => el.id === id)
+  cardByID: state => id => state.cards.find(card => card.id === Number(id))
 };
