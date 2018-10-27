7a042c0cc1e15cbe192d7746cb0a21b8183b3252
XoldpandaX <rembo-87@yandex.ru>
2018-10-27 23:23:16 +0700

    added new constants obj

---
+++ src/constants/confirmTexts.js
@@ -0,0 +1,7 @@
+export default {
+  unsavedCardData: {
+    title: 'If you go to another page then the data will not to be saved !\n Are you sure ?',
+    leftBtn: 'move and delete',
+    rightBtn: 'stay and save'
+  }
+};
